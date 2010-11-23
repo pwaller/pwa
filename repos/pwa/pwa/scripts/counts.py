94e55af69e3e6c173a911bd422573e6a4f4c6a35
Peter Waller <peter.waller@cern.ch>
2010-11-23 18:44:14 +0100

    Improvements to pwa.scripts.counts

--- repos/pwa/pwa/scripts/counts.py
+++ repos/pwa/pwa/scripts/counts.py
@@ -1,5 +1,6 @@
 from optparse import OptionParser
 from pprint import pprint
+from os.path import basename
 
 import minty
 
@@ -8,6 +9,10 @@ from minty.utils import comma_num
 from minty.utils.table_printer import pprint_table
 
 import ROOT as R
+
+aliases = dict(
+    high_pt="pt_gt40",
+)
 
 def do_all(files):
     for f in files:
@@ -21,17 +26,28 @@ def do_all(files):
 
 def do_cutflow_one_file(f, cuts):
     ph_cands = make_cut_histogram(f.photon_counts)
+    projax = ph_cands
     
     rows = []
-    for axis in ph_cands.axes_objs:
-        row = [axis.title, axis.true]
-        projax = axis.project_out()
+    for axis in [None] + ph_cands.axes_objs:
+        if axis:
+            title = aliases.get(axis.title, axis.title)
+            prevno = axis.true
+            projax = axis.project_out()
+        else:
+            title = "all"
+            prevno = ph_cands.hist.GetEntries()
+        row = [title, prevno]
+            
         for cut in cuts:
-            if cut == axis.title:
+            if axis and cut == axis.title:
                 row.append("-")
                 continue
             projax = projax(cut)
-            row.append(projax.true)
+            thisno = projax.true
+            #row.append("%.2f" % round(thisno / prevno * 100, 2) if prevno else 0)
+            row.append(thisno)
+            prevno = thisno
             projax = projax.project_out()
                 
         rows.append(row)
@@ -40,9 +56,17 @@ def do_cutflow_one_file(f, cuts):
 
 def do_cutflow(files, cuts):
     cuts = cuts.split()
-    header = ["cut", "all"] + cuts
+    header = ["cut", "all"] + [aliases.get(a, a) for a in cuts]
+
+    import pyfiglet
+    fig = pyfiglet.Figlet("/usr/share/figlet", font="standard", justify="center")
 
     for f in files:
+        period = basename(f.GetName())
+        print "-"*80
+        print fig.renderText(period)
+        print period.center(80)
+        print "-"*80
         table = do_cutflow_one_file(f, cuts)
         pprint_table([header] + table)
 
@@ -56,6 +80,6 @@ def main():
     files = [R.TFile(filename) for filename in input_filenames]
     
     #do_all(files)
-    do_cutflow(files, "isolated pt_gt100 oq pv grl loose robust_nontight robust_tight")
+    do_cutflow(files, "grl oq pv isolated loose robust_nontight robust_tight high_pt pt_gt100")
     
 
