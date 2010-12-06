243d9c68a825b7005ddfb0171762ba6778ec75b8
Peter Waller <pwaller@cern.ch>
2010-12-06 01:06:14 +0100

    Add bundle_build script

---
+++ bundle_build.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+source env/bin/activate
+if [ -e analysis.pybundle ]; then
+    rm analysis.pybundle
+fi 
+pip bundle analysis.pybundle repos/{minty,pytuple,OQMaps,PhotonIDTool,pwa}
+rm -rf env/build-bundle*
