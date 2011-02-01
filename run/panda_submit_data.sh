d308894cac18c724287640e2049516e0a866de71
Peter Waller <pwaller@cern.ch>
2011-02-01 13:03:16 +0100

    Update run scripts

--- run/panda_submit_data.sh
+++ run/panda_submit_data.sh
@@ -2,7 +2,7 @@
 
 source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda/panda_setup.sh
 
-PASS=23.5
+PASS=24.2
 
 prun                                                                             \
     --inDS user10.PeterWaller.PeriodAtoI.NTUP_PROMPTPHOT.p231/                   \
