94f254b200df468b24ec88a7ddcb693d427ad89d
Peter Waller <pwaller@cern.ch>
2011-03-07 16:44:04 +0100

    v16 grid submission script

---
+++ run/panda_submit_data_v16.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash 
+
+source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda/panda_setup.sh
+
+PASS=1
+
+prun                                                                             \
+    --inDS user.PeterWaller.PeriodDtoI.NTUP_PROMPTPHOT.p404/                   \
+    --outDS user.PeterWaller.purity.pau.v16.pass_$PASS.good.period.D.to.I/       \
+    --noBuild                                                                    \
+    --outputs output\*.root\*                                                    \
+    --nGBPerJob=5                                                                \
+    --writeInputToTxt=IN:inputs.txt                                              \
+    --exec './ana_run.sh --run-specific-output  -Gdata10_7TeV.pro05.merged_LBSUMM_eg_standard_7TeV.xml inputs.txt' \
+    --tmpDir /tmp/pwaller/pass.$PASS/                                            \
+    --athenaTag=16.0.2                                                           \
+    $@ 
