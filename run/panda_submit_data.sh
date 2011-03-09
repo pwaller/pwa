19b8e0b7ffe154b37abb7fb0cf7324c78c935957
Peter Waller <pwaller@cern.ch>
2011-03-09 12:22:57 +0100

    Add prepare_submit script, automatic tag detection

--- run/panda_submit_data.sh
+++ run/panda_submit_data.sh
@@ -2,16 +2,21 @@
 
 source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda/panda_setup.sh
 
-PASS=27
+if ! ./prepare_submit.sh; then
+  echo "Prepare submit failed."
+  exit 1
+fi;
+
+PASS=$(cat bundle_tag)
 
 prun                                                                             \
     --inDS user.PeterWaller.PeriodDtoI.NTUP_PROMPTPHOT.p231/                   \
-    --outDS user.PeterWaller.purity.pau.v15.pass_$PASS.good.period.A.to.I/       \
+    --outDS user.PeterWaller.purity.pau.v15.${PASS}.good.period.A.to.I/       \
     --noBuild                                                                    \
     --outputs output\*.root\*                                                    \
     --nGBPerJob=MAX                                                                \
     --writeInputToTxt=IN:inputs.txt                                              \
     --exec './ana_run.sh --run-specific-output  -Gdata10_7TeV.pro04.merged_LBSUMM_eg_standard_7TeV.xml inputs.txt' \
-    --tmpDir /tmp/pwaller/pass.$PASS/                                            \
+    --tmpDir /tmp/pwaller/pass.${PASS}/                                            \
     --athenaTag=16.0.2                                                           \
     $@ 
---
+++ run/prepare_submit.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+CURRENT_TAG=$(git describe --tags --exact-match --dirty 2> /dev/null)
+BUNDLE_TAG=$(cat bundle_tag)
+
+if [[ "x${CURRENT_TAG}" == "x" ]]; then 
+    echo "Getting the current tag failed"
+    exit 1
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Working tree is not tagged!"
+    exit 1
+fi
+
+if echo $CURRENT_TAG | grep dirty > /dev/null; then
+    echo "Working tree is dirty!"
+    exit 1
+fi
+
+echo Current tag is $CURRENT_TAG
+echo Bundle tag is $BUNDLE_TAG
+
+if [[ "$BUNDLE_TAG" != "$CURRENT_TAG" ]];
+then
+    echo "Rebuilding bundle.."
+    pushd .. > /dev/null
+    time ./bundle_build.sh > /dev/null
+    popd > /dev/null
+    echo $CURRENT_TAG > bundle_tag
+fi
+
