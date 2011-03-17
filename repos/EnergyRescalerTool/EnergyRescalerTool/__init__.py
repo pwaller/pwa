4d98c040a63dcc238b9fb23dce98161587c442ad
Peter Waller <pwaller@cern.ch>
2011-03-17 18:34:11 +0100

    Make EnergyRescaler use make_deffered_instance

--- repos/EnergyRescalerTool/EnergyRescalerTool/__init__.py
+++ repos/EnergyRescalerTool/EnergyRescalerTool/__init__.py
@@ -1,3 +1,13 @@
-from minty.utils import deferred_root_loader
+from minty.utils import deferred_root_loader, make_deferred_instance
 
 EnergyRescaler = deferred_root_loader("EnergyRescaler.cxx+", "EnergyRescaler")
+
+def initialize_EnergyRescaler(EnergyRescaler):
+    er = EnergyRescaler()
+    er.useDefaultCalibConstants()
+    return er
+
+energy_rescaler = make_deferred_instance(EnergyRescaler, initialize_EnergyRescaler)
+
+v16_E_correction = energy_rescaler.applyEnergyCorrection
+
