ca5b7174bbd288e613197f81c95eee35fd678aaf
Peter Waller <pwaller@cern.ch>
2011-03-22 14:28:18 +0100

    Update fetchdata script

--- run/fetch_data.sh
+++ run/fetch_data.sh
@@ -8,16 +8,20 @@ LAST_N=$(echo $LAST_TAG | cut -d'.' -f2)
 echo "Will fetch dataset for ${LAST_TAG} - ${LAST_N}"
 
 dest_dir=/data1/pwaller/v15/passes/${LAST_N}
-mkdir -p $dest_dir
+mkdir -p $dest_dir/mc
 pushd $dest_dir
-(dq2-get user.PeterWaller.purity.pau.v15.${LAST_TAG}.good.period.A.to.I/ 
- 2>&1 | mail -s "Done fetching v15.${LAST_TAG} (EOM)" pwaller) &
+((dq2-get user.PeterWaller.purity.pau.v15.${LAST_TAG}.good.period.A.to.I/)
+ 2>&1 | mail -s "Done fetching Data v15.${LAST_TAG} (EOM)" pwaller) &
+cd mc
+((dq2-get user.PeterWaller.pau.v15.all_mc.pass_${LAST_TAG}./
+user.PeterWaller.purity.pau.v15.${LAST_TAG}.good.period.A.to.I/)
+ 2>&1 | mail -s "Done fetching MC v15.${LAST_TAG} (EOM)" pwaller) &
 popd
 
 dest_dir=/data1/pwaller/v16/passes/${LAST_N}
 mkdir -p $dest_dir
 pushd $dest_dir
-(dq2-get user.PeterWaller.purity.pau.v16.${LAST_TAG}.good.period.D.to.I/
+((dq2-get user.PeterWaller.purity.pau.v16.${LAST_TAG}.good.period.D.to.I/)
  2>&1 | mail -s "Done fetching v16.${LAST_TAG} (EOM)" pwaller) &
 popd
 
