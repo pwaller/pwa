37f4960cf485e820c0ccd361800d191c6e4add8f
Peter Waller <peter.waller@cern.ch>
2011-05-03 11:47:57 +0100

    Make a most_recent.xml link

--- run/grls/update_grl.sh
+++ run/grls/update_grl.sh
@@ -3,5 +3,8 @@
 # Heuristic: Get the largest GRL. It's probably the most recent.
 
 P=/afs/cern.ch/user/a/atlasdqm/www/grlgen/CombinedPerf/Egamma/Eg_standard_v5
+F=$(ls -1S ${P}/data11* | head -n1)
 
-cp $(ls -1S ${P}/data11* | head -n1) .
+cp ${F} .
+
+ln -sf $(basename ${F}) most_recent.xml
