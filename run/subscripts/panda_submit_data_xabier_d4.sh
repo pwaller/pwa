b362a4fcb0adac5a02c7a2b27b813f500e1cf6c6
Peter Waller <peter.waller@cern.ch>
2011-05-25 11:28:04 +0100

    Move submission scripts into their own folder

---
+++ run/subscripts/panda_submit_data_xabier_d4.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash 
+
+set -u
+set -e
+
+source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda/panda_setup.sh
+
+if ! ./prepare_submit.sh; then
+  echo "Prepare submit failed."
+  exit 1
+fi;
+
+PASS=$(cat bundle_tag)
+EXTRA=$1
+
+prun                                                                            \
+    --inDS user.PeterWaller.skims.Xabier.D4.NTUP_PHOTON.f368_m806_p541.DR_Skim_v2/                       \
+    --outDS user.PeterWaller.skims.Xabier.D4.NTUP_PHOTON.f368_m806_p541.DR_Skim_v2.gravcount.${PASS}/    \
+    --noBuild                                                                   \
+    --outputs output\*.root\*                                                   \
+    --nGBPerJob=1                                                             \
+    --writeInputToTxt=IN:inputs.txt                                             \
+    --exec './ana_run.sh --run-specific-output -Ggrls/most_recent.xml inputs.txt' \
+    --tmpDir /tmp/pwaller/pass.${PASS}/                                         \
+    --athenaTag=16.6.3                                                          \
+    $@ 
