89dd239f3e187e16a58011c6c00a69d146067019
Peter Waller <peter.waller@cern.ch>
2011-05-27 15:31:17 +0100

    Add pwa-reduce, make mergeall smarter

--- repos/pwa/pwa/__init__.py
+++ repos/pwa/pwa/__init__.py
@@ -8,6 +8,9 @@ from commando import Application, command, subcommand, version, store, true, par
 
 from minty.main import make_main
 
+def mp_merge(args):
+    from hmerge import merge_files
+    merge_files(*args)
 
 class Engine(Application):
 
@@ -24,19 +27,53 @@ class Engine(Application):
         pass
 
     @subcommand('mergeall', help='Merge all of the files contained in a tgz')
-    @param('-f', '--file')
+    @param('files', nargs="+")
     def mergeall(self, params):
         from hmerge import merge_files
         from tarfile import open as tarfile_open
         from contextlib import closing
         
-        with closing(tarfile_open(params.file)) as tar:
-            files = set(f.path for f in tar.getmembers())
+        print "Input:", params.files
         
-        for f in files:
+        fileset = set()
+        for f in params.files:
+            with closing(tarfile_open(f)) as tar:
+                fileset |= set(rootfile.path for rootfile in tar.getmembers())
+        
+        for f in sorted(fileset):
             print "Building", f
-            merge_files(f, [params.file], pattern=f)
+            merge_files(f, params.files, pattern=f)
+    
+    @subcommand('reduce', help="Reduce many output- files to files by period.")
+    @store('-d', '--dummy')
+    def reduce(self, params):
+        from os import listdir
+        files = [f for f in listdir(".") 
+                 if f.startswith("output-") and f.endswith(".root")]
+        
+        def get_period(s): return s.split("-")[1].split("_")[-1]
+        
+        by_subperiod = {}
+        for f in files:
+            by_subperiod.setdefault(get_period(f), []).append(f)
             
+        from multiprocessing import Pool
+        pool = Pool(4)
+        
+        pool.map(mp_merge, [("period%s.root" % p, files) for p, files in by_subperiod.iteritems()])
+        
+        by_period = {}
+        for p, files in sorted(by_subperiod.iteritems()):
+            by_period.setdefault(p[0], []).extend(files)
+            
+        pool.map(mp_merge, [("period%s.root" % p, files) for p, files in by_period.iteritems()])
+        
+        from hmerge import merge_files
+        merge_files("all.root", ["period%s.root" % p for p in by_period])
+        
+        print "Done."
+
+    
     @subcommand('dump', help='Dump basic information')
     @param('files', nargs="+")
     def dump(self, params):
