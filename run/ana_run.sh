fb34add6c899b3d974e5dafe1d10a6492264942d
Peter Waller <peter.waller@cern.ch>
2011-06-02 17:10:21 +0100

    Make ana_run more robust by enforcing --never-download

--- run/ana_run.sh
+++ run/ana_run.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-./ana_bootstrap.py -v --distribute run_env
+./ana_bootstrap.py -v --distribute --never-download run_env
 source run_env/bin/activate
 
 python $(which pip) install analysis.pybundle
