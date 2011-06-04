edcba7adad8d37b188f4d2629fe7f070a4bd28ea
Peter Waller <peter.waller@cern.ch>
2011-06-04 10:36:49 +0100

    Add hmerge to analysis.pybundle

--- bundle_build.sh
+++ bundle_build.sh
@@ -4,5 +4,5 @@ source env/bin/activate
 if [ -e analysis.pybundle ]; then
     rm analysis.pybundle
 fi 
-pip bundle analysis.pybundle repos/{minty,pytuple,OQMaps,PhotonIDTool,pwa,EnergyRescalerTool,commando}
+pip bundle analysis.pybundle repos/{minty,pytuple,hmerge,OQMaps,PhotonIDTool,pwa,EnergyRescalerTool,commando}
 rm -rf env/build-bundle*
