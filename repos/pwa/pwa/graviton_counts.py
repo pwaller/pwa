f71bdb76649896dd73b11d334b290948c0685f8d
Peter Waller <peter.waller@cern.ch>
2011-07-14 12:33:41 +0100

    Make plots before any selection

--- repos/pwa/pwa/graviton_counts.py
+++ repos/pwa/pwa/graviton_counts.py
@@ -152,11 +152,15 @@ PHOTON_CUTFLOW = (
 def do_photon_cutflow(ana, event):
     counts = ana.h.get("photon_cutflow", b=[PHOTON_CUTFLOW])
     
+    good_photons = event.photons
+    
+    for ph in good_photons:
+        plot_kinematics(ana, "all_phs/pre_everything", ph)
+        plot_shower    (ana, "all_phs/pre_everything", ph)
+        
     # Fills first four bins of `counts`
     if not pass_event(counts, ana, event):
         return
-    
-    good_photons = event.photons
     
     if len(good_photons) < 2: return
     counts(PH_N)
